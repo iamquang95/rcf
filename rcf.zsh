adcc7094392b6d2a2e6a3a9a811070f2ff9a0733
Quang Le <iamquang95@gmail.com>
2021-01-19 10:11:12 +0700

    Update rcf.zsh

--- rcf.zsh
+++ rcf.zsh
@@ -2,7 +2,6 @@ rcf-append() {
   LBUFFER="${LBUFFER}$(cat /tmp/rcf.cmd)"
   local ret=$(cat /tmp/rcf.cmd)
   zle reset-prompt
-  return $ret
 }
 zle -N rcf-append
 bindkey '^R' rcf-append # Or whatever keybinding you want
